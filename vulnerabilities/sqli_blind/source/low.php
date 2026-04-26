1d942278c350987f5415d96126fc891d264ab56d
Mateusz <zabickimateusz64@gmail.com>
2026-04-26 17:43:44 +0200

    Naprawa błędów w kodzie

--- vulnerabilities/sqli_blind/source/low.php
+++ vulnerabilities/sqli_blind/source/low.php
@@ -1,46 +1,52 @@
 <?php
 
 if( isset( $_GET[ 'Submit' ] ) ) {
-	// Get input
-	$id = $_GET[ 'id' ];
+	// Check Anti-CSRF token
+	checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
 	$exists = false;
 
-	switch ($_DVWA['SQLI_DB']) {
-		case MYSQL:
-			// Check database
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			try {
-				$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ); // Removed 'or die' to suppress mysql errors
-			} catch (Exception $e) {
-				print "There was an error.";
-				exit;
-			}
+	// Get input
+	$id = $_GET[ 'id' ];
 
-			$exists = false;
-			if ($result !== false) {
-				try {
-					$exists = (mysqli_num_rows( $result ) > 0);
-				} catch(Exception $e) {
-					$exists = false;
+	// Was a number entered?
+	if(is_numeric( $id )) {
+		$id = intval ($id);
+		switch ($_DVWA['SQLI_DB']) {
+			case MYSQL:
+				// Check the database
+				$data = $db->prepare( 'SELECT first_name, last_name FROM users WHERE user_id = (:id) LIMIT 1;' );
+				$data->bindParam( ':id', $id, PDO::PARAM_INT );
+				$data->execute();
+
+				$exists = $data->rowCount();
+				break;
+			case SQLITE:
+				global $sqlite_db_connection;
+
+				$stmt = $sqlite_db_connection->prepare('SELECT COUNT(first_name) AS numrows FROM users WHERE user_id = :id LIMIT 1;' );
+				$stmt->bindValue(':id',$id,SQLITE3_INTEGER);
+				$result = $stmt->execute();
+				$result->finalize();
+				if ($result !== false) {
+					// There is no way to get the number of rows returned
+					// This checks the number of columns (not rows) just
+					// as a precaution, but it won't stop someone dumping
+					// multiple rows and viewing them one at a time.
+
+					$num_columns = $result->numColumns();
+					if ($num_columns == 1) {
+						$row = $result->fetchArray();
+
+						$numrows = $row[ 'numrows' ];
+						$exists = ($numrows == 1);
+					}
 				}
-			}
-			((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
-			break;
-		case SQLITE:
-			global $sqlite_db_connection;
+				break;
+		}
 
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			try {
-				$results = $sqlite_db_connection->query($query);
-				$row = $results->fetchArray();
-				$exists = $row !== false;
-			} catch(Exception $e) {
-				$exists = false;
-			}
-
-			break;
 	}
 
+	// Get results
 	if ($exists) {
 		// Feedback for end user
 		$html .= '<pre>User ID exists in the database.</pre>';
@@ -51,7 +57,9 @@ if( isset( $_GET[ 'Submit' ] ) ) {
 		// Feedback for end user
 		$html .= '<pre>User ID is MISSING from the database.</pre>';
 	}
-
 }
+
+// Generate Anti-CSRF token
+generateSessionToken();
 
 ?>
--- vulnerabilities/xss_r/source/low.php
+++ vulnerabilities/xss_r/source/low.php
@@ -1,11 +1,18 @@
 <?php
 
-header ("X-XSS-Protection: 0");
-
 // Is there any input?
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
+	// Check Anti-CSRF token
+	checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
+
+	// Get input
+	$name = htmlspecialchars( $_GET[ 'name' ] );
+
 	// Feedback for end user
-	$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>';
+	$html .= "<pre>Hello {$name}</pre>";
 }
+
+// Generate Anti-CSRF token
+generateSessionToken();
 
 ?>
